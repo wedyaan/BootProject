4aa2e6c976ec80252b96419185017c9458d8c1a4
wedyaan aloufi <wedyan-14@hotmail.com>
2023-09-18 12:37:39 +0300

    b

--- TeamProject/src/App.tsx
+++ TeamProject/src/App.tsx
@@ -1,5 +1,5 @@
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Stores from "./Pages/Stores";
 import Report from "./Pages/Report";
@@ -18,7 +18,10 @@ function App() {
         <Route path="/FriendlyProducts" element={<Chatbot></Chatbot>}></Route>
         <Route path="/store" element={<Stores></Stores>}></Route>
         <Route path="/report" element={<Report></Report>}></Route>
-        <Route path="/CarbonCal" element={<Cfpcalculator></Cfpcalculator>}></Route>
+        <Route
+          path="/CarbonCal"
+          element={<Cfpcalculator></Cfpcalculator>}
+        ></Route>
         <Route path="/mapco" element={<MapComponent></MapComponent>}></Route>
       </Routes>
       <Footer></Footer>
